43d09330dce0330705eb97440b9952957da7d797
Rohit Sharma <saskaeval@gmail.com>
2024-11-04 18:45:57 +0530

    portfolio completed

---
+++ WebDevelopment/Portfolio/script.js
@@ -0,0 +1,9 @@
+// script.js
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
+
+
